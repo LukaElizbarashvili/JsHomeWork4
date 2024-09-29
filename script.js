60e29e017c8113eb07a5e42e56fc00f27e7e47c7
LukaElizbarashvili <elizbarashvililuka3@gmail.com>
2024-09-29 20:21:47 +0400

    almost done

---
+++ script.js
@@ -0,0 +1,62 @@
+// Task one
+let fruits = ["apple", "mango", "avocado", "kiwi"];
+fruits.splice(fruits.length-2,1, "strawberry");
+console.log(fruits);
+
+// Task Two
+
+// Task Three
+function string(theString) {
+    return theString.length;
+}
+let stringLenght = string("apple")
+console.log(stringLenght);
+
+// Task Three
+let info = "good Day";
+let newInfo = info.slice(5,8);
+console.log(newInfo);
+
+// Task Four
+let array1 = [5, 25, 89, 120, 36];
+array1.push("javascrips", "python");
+array1.unshift("html", "css");
+console.log(array1.length);
+array1.shift();
+array1.pop();
+
+console.log(array1);
+
+// Task Fivc
+
+let array2 = ["ფორთოხალი", "ბანანი", "კიvი"]
+console.log(array2.length);
+array2.push("ვაშლი", "ანანასი");
+array2.unshift("საზამთრო");
+console.log(array2.length);
+array2.splice(2,0, "მანგო");
+array2.shift();
+array2.pop();
+console.log(array2);
+console.log(array2.length);
+
+// Task Six
+let array3 = [1, 2, 3, 4, 5];
+array3.splice(3,0, "a", "b", "c")
+console.log(array3);
+
+// Task Seven
+let array7 =  [15, 100, 25, 10, 36];
+array7.splice(3,1)
+console.log(array7);
+
+// Task eight
+let array8 = [ 1, 2 , 3 , 4, 5];
+array8.splice(2,1, "three");
+console.log(array8);
+
+// Task Nine
+
+
+
+
